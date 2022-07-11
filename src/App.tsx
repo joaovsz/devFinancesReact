c7e60ffaa1128baa4150373791546fb357e8b24e
joaovsz <joaovitor1713coin@gmail.com>
2022-07-11 10:43:27 -0300

    modal removido

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,14 @@
-import {useSelector } from "react-redux"
 import { Cards } from "./components/Cards"
 import { Footer } from "./components/Footer"
 import { Header } from "./components/Header"
-import { Modal } from "./components/Modal"
 import { Table } from "./components/Table"
 
 function App() {
-  const {isOpen} = useSelector((store:any) => store.transactions);
-
+  
   return <div>
     <Header />
     <Cards />
-    {isOpen &&<Modal/>}
+   
     <Table />
     <Footer />
   </div>
