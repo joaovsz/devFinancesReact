5c12e4a7a1de1daf57ee30cff0559f17bc154748
joaovsz <joaovitor1713coin@gmail.com>
2022-07-18 11:40:11 -0300

    persistStorage com redux-persist

--- src/components/redux/store.ts
+++ src/components/redux/store.ts
@@ -1,11 +1,22 @@
 import {configureStore} from '@reduxjs/toolkit'
+import {persistStore, persistReducer} from 'redux-persist'
 import stockReducer from './transactionSlice'
+import storage from 'redux-persist/lib/storage'
 
+const persistConfig = {
+  key:'root',
+  storage,
+}
 
+const persistedReducer = persistReducer(persistConfig, stockReducer)
 
-export const store = configureStore({
+const store = configureStore({
   reducer:{
-    transactions:  stockReducer
+    transactions:  persistedReducer
   }
 })
+const persistor = persistStore(store)
+
+export  {store, persistor}
+
 export type RootState = ReturnType<typeof  store.getState>
