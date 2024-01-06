161ed60b57a154b3b42276a6876b1d376286ebf3
joaovsz <joaovitor1713coin@gmail.com>
2024-01-06 18:25:15 -0300

    Correção decimais

--- src/components/Transactions.tsx
+++ src/components/Transactions.tsx
@@ -3,22 +3,25 @@ import { Transaction } from "../types/transaction"
 import MinusIcon from "./icons/MinusIcon"
 import { minusTransaction, removeTransaction } from './redux/transaction-slice'
 
-export const formatCurrency = (value:number|string)=>{
-    const signal = value<0?"-":""
-    //Regex 
-    value = String(value).replace(/\D/g,"") 
-    value = Number(value).toLocaleString("pt-br", {
-      style: "currency",
-      currency: "BRL"
-  })
-  return signal + value
-  }
+export const formatCurrency = (value: number | string) => {
+  const signal = Number(value) < 0 ? "-" : "";
+
+  // Regex para remover strings não numericas
+  const numericValue = String(value).replace(/\D/g, "");
+
+  const formattedValue = (Number(numericValue) / 100).toLocaleString("pt-br", {
+    style: "currency",
+    currency: "BRL"
+  });
+
+  return signal + formattedValue;
+};
+
+
 
 const Transactions = () => {
   const transactions = useSelector((store:any)=>store.transactions.transaction)
   const dispatch = useDispatch()
-  
- 
 
   function removeTransactions(id:string, type:number, amount:number){
     dispatch(removeTransaction(id))
